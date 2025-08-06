7ca8f4533f03c0598960843dac774152adb96ef5
Harsh pandey <thepandey144@gmail.com>
2025-08-06 14:26:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import ViewProfilePage from "@/pages/ViewProfilePage";
 
 const queryClient = new QueryClient();
 
@@ -20,7 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/:id" element={<ProfileViewPage />} />
+          <Route path="/profile/:id" element={<ViewProfilePage />} />
 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
